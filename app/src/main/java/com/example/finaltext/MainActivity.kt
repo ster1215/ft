ffa2c4780b11f8abdcd9ca174709b4bf3b0f666b
s1091755 <s1091755@github.com>
2022-05-30 15:13:09 +0800

    v.3

--- app/src/main/java/com/example/finaltext/MainActivity.kt
+++ app/src/main/java/com/example/finaltext/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.finaltext
 
 
-import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var game : MySurfaceView
     var flag:Boolean = false
     lateinit var job : Job
+    lateinit var imgAuthor : ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,15 @@ class MainActivity : AppCompatActivity() {
 
         img = findViewById(R.id.img)
         game = findViewById(R.id.mysv)
+
+        imgAuthor = findViewById(R.id.imgAuthor)
+        GlideApp.with(this)
+            //.load(R.drawable.earth)
+            .load(R.drawable.taper)
+            .circleCrop()
+            .override(800, 600)
+            .into(imgAuthor)
+
 
         img.setOnClickListener({
             if (flag){
